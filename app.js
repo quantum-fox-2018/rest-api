157b0a209edc8a1c7b35fd58c8ea31cd907b7147
Dani Damara <danny12march@gmail.com>
2018-04-02 20:26:19 +0700

    Install npm morgan into app

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 const port = process.env.PORT || 4000;
 //const routes = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+app.use(morgan('dev'));
 
 let api = require('./routes/api');
 app.use('/api', api);
