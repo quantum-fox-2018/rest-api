0ebd613d9c39591186f54050ed9be35132a6c313
Fitrul Islam <fitrul.islam@gmail.com>
2018-04-02 19:22:20 +0700

    add database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-const PORT = 3000
+const port = process.env.PORT || 3000
 const index = require('./routes/index')
 const api = require('./routes/api')
 
@@ -13,6 +13,6 @@ app.use(morgan('dev'))
 app.use('/', index)
 app.use('/api', api)
 
-app.listen(PORT,function(req,res){
+app.listen(port,function(req,res){
   console.log(`App listen on ${PORT}`)
 })
