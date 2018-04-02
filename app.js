1f82abf204aae8361018f867cc0035fa9223f93f
WikaSilo <wikanyaa@gmail.com>
2018-04-02 20:02:48 +0700

    fixed homepage

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json())
 app.use(express.urlencoded( {extended: false} ))
 app.use(morgan('tiny'))
 
-app.use('/api', require('./routes'))
+app.use('/', require('./routes'))
 
 app.listen(port, () => {
   console.log(`its running on ${port}`)
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,12 @@
 const routes = require('express').Router()
 
-const apiController = require('../controllers/api')
-const auth = require('../middleware/auth')
-
 routes.get('/', (req, res) => {
   res.status(200).json({
-    message: "hello world"
+    message: "hai, let go the /api!"
   })
 })
 
-routes.post('/signup', apiController.signupUser)
-routes.post('/signin', apiController.signinUser)
-
+routes.use('/api', require('./api'))
 routes.use('/users', require('./users'))
 
 module.exports = routes
